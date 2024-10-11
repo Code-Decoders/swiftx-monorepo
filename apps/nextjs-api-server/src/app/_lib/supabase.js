a1beb5697ef8cc64407049ad8a5c28d3ec615ab2
kunal528 <64953335+kunal528@users.noreply.github.com>
2024-10-12 00:30:29 +0530

    Refactor app launcher icons, postcss.config.js, tsconfig.json, tailwind.config.js, and package.json

---
+++ apps/nextjs-api-server/src/app/_lib/supabase.js
@@ -0,0 +1,65 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabase = createBrowserClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+)
+
+export const signIn = async (email, password) => {
+    const { data: {
+        user
+    }, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+    if (error) throw error;
+    return user;
+}
+
+export const logout = async () => {
+    await supabase.auth.signOut();
+}
+
+export const getCurrentUser = () => {
+    return supabase.auth.getUser();
+}
+
+export const getUserData = async () => {
+    const { data: { user } } = await getCurrentUser();
+    const { data, error } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', user.id).single();
+    if (error) throw error;
+    return data;
+}
+
+export const getTransactions = async () => {
+    const { data: { user } } = await getCurrentUser();
+    const { data: userData, } = await supabase
+        .from('users')
+        .select('*')
+        .eq('id', user.id).single();
+
+    const { data, error } = await supabase
+        .from('transactions')
+        .select('*, receiver:receiver_id(*)')
+        .eq("receiver.country", userData.country)
+        .eq('status', "completed")
+        .not('receiver', 'is', null)
+        .order('created_at', { ascending: false });
+    if (error) throw error;
+    return data;
+}
+
+export const burnToken = async (id) => {
+    const { error } = await supabase
+        .from('transactions')
+        .update({ status: "burned" })
+        .eq('id', id);
+    if (error) throw error;
+    return true;
+}
+
+
+
--- apps/nextjs-api-server/src/app/page.tsx
+++ apps/nextjs-api-server/src/app/page.tsx
@@ -1,95 +1,57 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// pages/index.js
+"use client";
+import { useEffect, useState } from "react";
+import Table from "./components/Table";
+import { burnToken, getTransactions, logout } from "./_lib/supabase";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [tableData, setTableData] = useState<any[]>([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  const router = useRouter();
+
+  useEffect(() => {
+    getTransactions().then((data) => {
+      setTableData(data);
+    });
+  }, []);
+
+  const onBurn = async (id: string) => {
+    burnToken(parseInt(id));
+    const data = await getTransactions();
+    setTableData(data);
+  };
+
+  return (
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center">
+      <nav className="w-full bg-white shadow-md mb-6">
+        <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+          <div className="text-xl font-semibold text-gray-700">SwiftX</div>
+          <button
+            className="bg-primary text-white px-4 py-2 rounded hover:bg-blue-600"
+            onClick={() => {
+              logout();
+              router.push("/login");
+            }}
           >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+            Logout
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </nav>
+      <div className="w-full max-w-7xl">
+        <h1 className="text-3xl font-semibold text-gray-700 mb-6 text-left">
+          Hi Admin,
+        </h1>
+        <p className="text-lg text-gray-700 mb-6 text-left">
+          Here are the transactions that have been made.
+        </p>
+        <Table
+          data={tableData}
+          onBurnToken={(transaction: any) => {
+            onBurn(transaction.id);
+          }}
+        />
+      </div>
     </div>
   );
 }
