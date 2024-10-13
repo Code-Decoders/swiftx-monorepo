c31733d8770d2a47a6ffbc6cea629948d4f06293
kunal528 <64953335+kunal528@users.noreply.github.com>
2024-10-13 15:25:20 +0530

    Refactor login and home pages to include footer with developer information

--- apps/nextjs-api-server/src/middleware.js
+++ apps/nextjs-api-server/src/middleware.js
@@ -21,11 +21,11 @@ export async function middleware(request) {
         },
         setAll(cookiesToSet) {
           cookiesToSet.forEach(({ name, value, options }) => request.cookies.set(name, value))
-          supabaseResponse = NextResponse.next({
+          response = NextResponse.next({
             request,
           })
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+            response.cookies.set(name, value, options)
           )
         },
       },
@@ -73,12 +73,12 @@ export async function middleware(request) {
       return NextResponse.redirect(url)
     }
 
-    // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
+    // IMPORTANT: You *must* return the response object as it is. If you're
     // creating a new response object with NextResponse.next() make sure to:
     // 1. Pass the request in it, like so:
     //    const myNewResponse = NextResponse.next({ request })
     // 2. Copy over the cookies, like so:
-    //    myNewResponse.cookies.setAll(supabaseResponse.cookies.getAll())
+    //    myNewResponse.cookies.setAll(response.cookies.getAll())
     // 3. Change the myNewResponse object to fit your needs, but avoid changing
     //    the cookies!
     // 4. Finally:
