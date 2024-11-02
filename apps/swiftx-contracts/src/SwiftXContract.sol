46552fb9de060c39bddff089b98d7ce443cdba65
Rana Satyam Raj <ranasatyamraj@gmail.com>
2024-11-02 18:47:23 +0530

    Update SwiftXContract.sol

--- apps/swiftx-contracts/src/SwiftXContract.sol
+++ apps/swiftx-contracts/src/SwiftXContract.sol
@@ -2,7 +2,8 @@
 pragma solidity ^0.8.13;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../lib/wormhole-solidity-sdk/src/interfaces/IWormholeReceiver.sol";
+import "./lib/wormhole-solidity-sdk/src/interfaces/IWormholeReceiver.sol";
+import "./lib/wormhole-solidity-sdk/src/interfaces/IWormholeRelayer.sol";
 
 /**
  * @title SwiftX
@@ -15,11 +16,20 @@ contract SwiftX is IWormholeReceiver, Ownable {
         uint256 amount;
     }
 
+    IWormholeRelayer wormholeRelayer;
+
+    uint256 GAS_LIMIT = 250_000;
+
     mapping(uint256 => SwiftXMetadata) public ledger;
+    mapping(address => uint16) public childTokenToChainId;
 
     event LedgerUpdated(uint256 indexed nonce, SwiftXMetadata txn);
 
-    constructor() Ownable(msg.sender){}
+    constructor(
+        address _wormholeRelayer
+    ) Ownable(msg.sender){
+        wormholeRelayer = IWormholeRelayer(_wormholeRelayer);
+    }
 
     /**
      * @notice Handles incoming Wormhole messages and updates the ledger.
@@ -29,7 +39,7 @@ contract SwiftX is IWormholeReceiver, Ownable {
     function receiveWormholeMessages(
         bytes memory payload,
         bytes[] memory,
-        bytes32,        
+        bytes32 sourceAddress,        
         uint16,
         bytes32             
     ) public payable override {                 
@@ -37,15 +47,45 @@ contract SwiftX is IWormholeReceiver, Ownable {
         (uint256 txId, uint256 amount, address recipient, address sender) = abi
             .decode(payload, (uint256, uint256, address, address));
 
+        address childToken = address(uint160(uint256(sourceAddress)));
+
         SwiftXMetadata memory txn = SwiftXMetadata({
             sender: sender,
             recipient: recipient,
             amount: amount
         });
 
+        // Encode the payload: (amount, sender)
+        bytes memory tokenPayload = abi.encode(
+            txId,
+            amount,
+            recipient
+        );
+
+        uint16 childChainId = childTokenToChainId[childToken];
+
+        uint256 cost = quoteCrossChainDeposit(childChainId);
+
+        wormholeRelayer.sendPayloadToEvm{value: cost}(
+            childChainId,
+            childToken,
+            tokenPayload,
+            0,
+            GAS_LIMIT
+        );
+        
         ledger[txId] = txn;
 
         emit LedgerUpdated(txId, txn);
+    }
+
+    function addChildTokens(address tokenAddress, uint16 chainId) public onlyOwner {
+        childTokenToChainId[tokenAddress] = chainId;
+    }
+
+    function getChildTokens(address tokenAddress) 
+        public view returns (uint16 childChainId){
+            return childTokenToChainId[tokenAddress];
     }
 
     function getTransaction(
@@ -53,5 +93,15 @@ contract SwiftX is IWormholeReceiver, Ownable {
     ) public view returns (SwiftXMetadata memory) {
         SwiftXMetadata memory metadata = ledger[txId];
         return metadata;
+    }
+
+    function quoteCrossChainDeposit(uint16 targetChain) public view returns (uint256 cost) {
+        uint256 deliveryCost;
+        (deliveryCost,) = wormholeRelayer.quoteEVMDeliveryPrice(
+            targetChain,
+            0,
+            GAS_LIMIT
+        );
+        return deliveryCost;
     }
 }
